d6548b3c1ec25de33918b330a44e45d8b101b8c2
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-13 23:29:11 +0300

    SLICE up on px change

--- Js/script.js
+++ Js/script.js
@@ -64,6 +64,26 @@ window.onload = () => {
       speakersList.insertAdjacentHTML('afterbegin', speakerHTML);
     });
   };
-
+window.addEventListener('resize', () => {
+  document.getElementById('more-btn').classList.remove('hide');
+  speakersList.innerHTML = '';
+  if (window.screen.width < 768) {
+    speakersToShow = speakers.slice(0, 2);
+  } else {
+    speakersToShow = speakers;
+  }
+  speakersToShow.forEach((speaker) => {
+    const speakerHTML = `<article class="speaker clearfix">
+                                <img src=${speaker.picture} alt="${speaker.name}"/>
+                                <div class="desc">
+                                    <p href="#" class="speaker-name">${speaker.name}</p>
+                                    <p href="#" class="speaker-tag-name">${speaker.tag_name}</p>
+                                    <p href="#" class="speaker-job">${speaker.workplace}</p>
+                                    <p class="speaker-desc">${speaker.description}</p>
+                                </div>
+                        </article>`;
+    speakersList.insertAdjacentHTML('afterbegin', speakerHTML);
+  });
+});
   
 console.log('hi mate')
